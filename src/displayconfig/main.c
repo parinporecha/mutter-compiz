b0ed372cb8a63fdafde1d1d26c82429c2ee259f1
Tim Lunn <tim@feathertop.org>
2014-01-08 13:50:10 +1100

    initial build scripts for standalone display config

---
+++ src/displayconfig/main.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+
+#include <glib.h>
+
+
+#include <gtk/gtk.h>
+
+#ifdef GDK_WINDOWING_X11
+#include <X11/Xlib.h>
+#endif
+
+
+
+int main (int argc, char ** argv) {
+        return 0;
+}
+
